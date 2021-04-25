2091125e0f453ebeb4ce7b40dfeae1d9129dbd59
DeveloperRooney <han_0427@naver.com>
2021-04-25 21:29:26 +0900

    update

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-from slacker import Slacker
+# from slacker import Slacker
 
-slack = Slacker('xoxb-1998689688754-1983918473255-JdW0XKHCvRyqL05Cbaf58002')
+# slack = Slacker('xoxb-1998689688754-1983918473255-Kn9HFKKecCuj9g9rX2F7nMTK')
 
-# Send a message to #general channel
-slack.chat.post_message('#stock', 'Hello fellow slackers!')
+# # Send a message to #general channel
+# slack.chat.post_message('#stock', 'Hello fellow slackers!')
+
+import requests
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization" : "Bearer " + token},
+        data={"channel" : channel, "text" : text}
+    )
+    print(response)
+
+myToken = "xoxb-1998689688754-1983918473255-Kn9HFKKecCuj9g9rX2F7nMTK"
+
+post_message(myToken, "#stock", "suhan")
